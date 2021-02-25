2a3000f9b8518d54b7b521b5ff25d433f84218df
Adam553882 <adam.domagala111@gmail.com>
2021-02-25 18:28:11 +0100

    Directory problem fix

--- sorting_script.sh
+++ sorting_script.sh
@@ -9,7 +9,27 @@ mv ~/Downloads/*.png ~/Pictures/
 
 mv ~/Downloads/*.mp4 ~/Videos/
 
-mv ~/Downloads/*.gcode ~/3D\ Printing/Gcode\ files/
-mv ~/Downloads/*.STL ~/3D\ Printing/STL\ files/
+Printing_Dir=~/3D\ Printing
+if test -d "$Printing_Dir"; then
 
+else
+        mkdir ~/3D\ Printing
+fi
 
+gcode=~/3D\ Printing/~/3D\ Printing/Gcode\ files/
+
+if test -d "$gcode"; then
+        mv ~/Downloads/*.gcode ~/3D\ Printing/Gcode\ files/
+else
+        mkdir ~/3D\ Printing
+        mv ~/Downloads/*.gcode ~/3D\ Printing/Gcode\ files/
+fi
+
+stl=~/Downloads/*.STL ~/3D\ Printing/STL\ files/
+
+if test -d "$stl"; then
+        mv ~/Downloads/*.STL ~/3D\ Printing/STL\ files/
+else
+        mkdir ~/3D\ Printing
+        mv ~/Downloads/*.STL ~/3D\ Printing/STL\ files/
+fi
