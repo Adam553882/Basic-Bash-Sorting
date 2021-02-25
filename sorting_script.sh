ba3a5891f4c0fb48a7741b4ae5013086a9dda5e0
Adam553882 <adam.domagala111@gmail.com>
2021-02-25 17:33:21 +0100

    First Commit

---
+++ sorting_script.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+mv ~/Downloads/*.txt ~/Documents/
+mv ~/Downloads/*.pdf ~/Documents/
+mv ~/Downloads/*.doc ~/Documents/
+
+mv ~/Downloads/*.jpg ~/Pictures/
+mv ~/Downloads/*.png ~/Pictures/
+
+mv ~/Downloads/*.mp4 ~/Videos/
+
+mv ~/Downloads/*.gcode ~/3D\ Printing/Gcode\ files/
+mv ~/Downloads/*.STL ~/3D\ Printing/STL\ files/
+
+
